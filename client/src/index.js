852a4d234e41656f91dceb485b0ec2b1f431299f
jemchen02 <90587179+jemchen02@users.noreply.github.com>
2022-08-02 17:01:35 -0400

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Classroom from './components/classroom.js';
+
+ReactDOM.render(
+  <Classroom />,
+  document.getElementById('root')
+);
